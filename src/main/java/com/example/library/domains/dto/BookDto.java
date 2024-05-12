7dcef0375874dd82282a5ac299be3dcac9d50355
anish <anish.diliyan@gmail.com>
2024-05-12 16:48:49 +0530

    create book api added

--- src/main/java/com/example/library/domains/dto/BookDto.java
+++ src/main/java/com/example/library/domains/dto/BookDto.java
@@ -3,7 +3,10 @@ package com.example.library.domains.dto;
 
 import lombok.*;
 
-
+//Builder needed All arg constructor
+@AllArgsConstructor
+// to build object using builder pattern in TestDataUtil
+@Builder
 //No @Setter: you will get null values in response
 @Setter
 //No @Getter: BookEntity must be manually assigned before calling 'persist()'
--- src/test/java/com/example/library/TestDataUtil.java
+++ src/test/java/com/example/library/TestDataUtil.java
@@ -1,9 +1,13 @@
 package com.example.library;
 
 import com.example.library.domains.dto.AuthorDto;
+import com.example.library.domains.dto.BookDto;
 
 public class TestDataUtil {
     public static AuthorDto createAuthor() {
         return AuthorDto.builder().id(1L).name("Anish").age(20).build();
+    }
+    public static BookDto createBook() {
+        return BookDto.builder().isbn("97-98-99").title("The Whale").author(createAuthor()).build();
     }
 }
