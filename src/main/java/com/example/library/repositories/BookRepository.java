ea267b0bb79fe3c032c7905ef35846a5df29424e
anish <anish.diliyan@gmail.com>
2024-05-12 18:00:46 +0530

    book controller test passed

--- src/main/java/com/example/library/repositories/BookRepository.java
+++ src/main/java/com/example/library/repositories/BookRepository.java
@@ -3,9 +3,13 @@ package com.example.library.repositories;
 
 import com.example.library.domains.entities.BookEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface BookRepository extends CrudRepository<BookEntity, String> {
+public interface BookRepository extends
+        CrudRepository<BookEntity, String>,
+        PagingAndSortingRepository<BookEntity, String> {
+
 }
--- src/main/java/com/example/library/services/BookService.java
+++ src/main/java/com/example/library/services/BookService.java
@@ -1,7 +1,24 @@
 package com.example.library.services;
 
 import com.example.library.domains.entities.BookEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface BookService {
-    BookEntity createBook(BookEntity book);
+    BookEntity createUpdateBook(String isbn, BookEntity book);
+
+    List<BookEntity> findAll();
+
+    Page<BookEntity> findAll(Pageable pageable);
+
+    Optional<BookEntity> findOne(String isbn);
+
+    boolean isExists(String isbn);
+
+    BookEntity partialUpdate(String isbn, BookEntity bookEntity);
+
+    void delete(String isbn);
 }
